997c496b641d890d02f5c6d351d7c1342855fdc2
Michael Carr <michael.carr@outlook.com>
2016-05-04 00:36:31 +0100

    rubbish attempt at primes

--- prime_numbers/lib/prime_numbers.ex
+++ prime_numbers/lib/prime_numbers.ex
@@ -2,12 +2,29 @@ defmodule PrimeNumbers do
   def generate(0) do
     ""
   end
+
+  def generate(1) do
+    [2]
+  end
+
   def generate(number_to_generate) do
-    cond do
-      number_to_generate == 0 -> ""
-      number_to_generate == 1 -> [2]
-      number_to_generate == 2 -> [2, 3]
-      number_to_generate == 3 -> [2, 3, 5]
+    generate_recurssive(number_to_generate, [2], 1)
+  end
+
+  defp generate_recurssive(no_of_requested_primes, result, index) do
+    if no_of_requested_primes == Enum.count(result) do
+      result
+    else
+      potential_prime = index + 2
+      cond do
+        is_prime?(potential_prime, result) == true -> generate_recurssive(no_of_requested_primes, result ++ [potential_prime], potential_prime)
+        is_prime?(potential_prime, result) == false -> generate_recurssive(no_of_requested_primes, result, potential_prime)
+      end
     end
   end
+
+  def is_prime?(potential_prime, primes) do
+    Enum.any?(primes, fn(prime) -> rem(potential_prime, prime) == 0 end) == false
+  end
+
 end
--- prime_numbers/test/prime_numbers_test.exs
+++ prime_numbers/test/prime_numbers_test.exs
@@ -17,4 +17,28 @@ defmodule PrimeNumbersTest do
     assert PrimeNumbers.generate(3) == [2, 3, 5]
   end
 
+  test "when generating 5 primes i get the first five primes" do
+    assert PrimeNumbers.generate(5) == [2, 3, 5, 7, 11]
+  end
+
+  test "when generating 10 primes i get the first ten primes" do
+    assert PrimeNumbers.generate(10) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+  end
+
+  test "when finding if 3 is prime returns true" do
+    assert PrimeNumbers.is_prime?(3, [2]) == true
+  end
+
+  test "when finding if 5 is prime returns true" do
+    assert PrimeNumbers.is_prime?(5, [2, 3]) == true
+  end
+
+  test "when finding if 7 is prime returns true" do
+    assert PrimeNumbers.is_prime?(7, [2, 3, 5]) == true
+  end
+
+  test "when finding if 9 is prime returns false" do
+    assert PrimeNumbers.is_prime?(9, [2, 3, 5, 7]) == false
+  end
+
 end
