5f8b603c6557bc8f9953e222ce311848db441a9b
Michael Carr <michael.carr@outlook.com>
2016-05-03 23:29:04 +0100

    adding pattern matching on the zero case

--- prime_numbers/lib/prime_numbers.ex
+++ prime_numbers/lib/prime_numbers.ex
@@ -1,8 +1,13 @@
 defmodule PrimeNumbers do
+  def generate(0) do
+    ""
+  end
   def generate(number_to_generate) do
     cond do
       number_to_generate == 0 -> ""
       number_to_generate == 1 -> [2]
+      number_to_generate == 2 -> [2, 3]
+      number_to_generate == 3 -> [2, 3, 5]
     end
   end
 end
--- prime_numbers/test/prime_numbers_test.exs
+++ prime_numbers/test/prime_numbers_test.exs
@@ -8,4 +8,13 @@ defmodule PrimeNumbersTest do
   test "when generating 1 primes i get the first prime" do
     assert PrimeNumbers.generate(1) == [2]
   end
+
+  test "when generating 2 primes i get the first two primes" do
+    assert PrimeNumbers.generate(2) == [2, 3]
+  end
+
+  test "when generating 3 primes i get the first three primes" do
+    assert PrimeNumbers.generate(3) == [2, 3, 5]
+  end
+
 end
