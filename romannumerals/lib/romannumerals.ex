261a95f462cdbb630d00d874836ce5a3f2511570
Michael Carr <mcarr@findmypast.com>
2016-05-06 11:20:11 +0100

    fixing the code a little

--- romannumerals/lib/romannumerals.ex
+++ romannumerals/lib/romannumerals.ex
@@ -1,22 +1,22 @@
 defmodule RomanNumerals do
 
-@arabic_conversions %{50 =>"L", 10 => "X", 5 => "V", 1 => "I"}
+@arabic_conversions [[50, "L"],  [10, "X"],  [5, "V"],  [1, "I"]]
+
 
   def to_roman_numerals(input_number) do
-    recursive_roman_numerals(input_number, "", Enum.reverse(@arabic_conversions))
+    recursive_roman_numerals(@arabic_conversions, input_number, "")
   end
 
-  defp recursive_roman_numerals(input_number, final_result, conversion ) do
-    Enum.reduce conversion, fn({arabic, roman}, acc) ->
-      cond do
-        input_number >= arabic ->
-          recursive_roman_numerals(input_number - arabic, roman <> final_result, acc)
-        arabic > 0 ->
-          recursive_roman_numerals(input_number, final_result, acc)
-        true ->
-          "poo"
-      end
-    end
+  defp recursive_roman_numerals( [], 0, final_result ), do: final_result
 
+  defp recursive_roman_numerals( [ head | rest], input_number, final_result ) do
+    arabic_number = List.first(head)
+    roman_character = List.last(head)
+    cond do
+      input_number >= arabic_number ->
+        recursive_roman_numerals([ head | rest], input_number - arabic_number, final_result <> roman_character)
+      :else > 0 ->
+        recursive_roman_numerals(rest, input_number, final_result)
+    end
   end
 end
